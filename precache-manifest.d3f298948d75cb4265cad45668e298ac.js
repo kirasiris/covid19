871a483884ec89e9580d25ef0a4b4ca35bd63fdd
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2020-03-28 00:17:36 -0500

    Updates

---
+++ precache-manifest.d3f298948d75cb4265cad45668e298ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26222a3791a91682c90880ab1a90dd79",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "81ce02344fe5ccb78221",
+    "url": "/covid19/static/css/main.6c505969.chunk.css"
+  },
+  {
+    "revision": "f107bf80aa34468fb155",
+    "url": "/covid19/static/js/2.f5e1c7ee.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/covid19/static/js/2.f5e1c7ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ce02344fe5ccb78221",
+    "url": "/covid19/static/js/main.eecd9863.chunk.js"
+  },
+  {
+    "revision": "cbcc706a45298e621d9c",
+    "url": "/covid19/static/js/runtime-main.93b84886.js"
+  }
+]);
